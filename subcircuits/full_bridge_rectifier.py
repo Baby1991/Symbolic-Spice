e06daf9f122bbb984a5cae2fa8ea40f330a3b263
Filip Stevanovic <41167212+Baby1991@users.noreply.github.com>
2024-09-10 23:44:02 -0400

    rectifier fix

--- subcircuits/full_bridge_rectifier.py
+++ subcircuits/full_bridge_rectifier.py
@@ -1,20 +1,11 @@
 from subcircuits.subcircuits import *
 
 full_bridge_rectifier = Circuit()
-Vd = full_bridge_rectifier.generic("Vd", 0.6)
+#Vd = full_bridge_rectifier.generic("Vd", 0.6)
+Vd = full_bridge_rectifier.constant("Vd", 0.6)
 full_bridge_rectifier.element(
                                 Diode("D1", {"Vp" : "Vin1", "Vn" : "Vout+"}, Vd=Vd),
                                 Diode("D2", {"Vp" : "Vout-", "Vn" : "Vin2"}, Vd=Vd),
                                 Diode("D3", {"Vp" : "Vout-", "Vn" : "Vin1"}, Vd=Vd),
                                 Diode("D4", {"Vp" : "Vin2", "Vn" : "Vout+"}, Vd=Vd),
-)
-
-semi_real_full_bridge_rectifier = Circuit()
-Vd = semi_real_full_bridge_rectifier.generic("Vd", 0.6)
-Is = semi_real_full_bridge_rectifier.generic("Is", 1e-9)
-semi_real_full_bridge_rectifier.element(
-                                SemiRealDiode("D1", {"Vp" : "Vin1", "Vn" : "Vout+"}, Vd=Vd, Is=Is),
-                                SemiRealDiode("D2", {"Vp" : "Vout-", "Vn" : "Vin2"}, Vd=Vd, Is=Is),
-                                SemiRealDiode("D3", {"Vp" : "Vin2", "Vn" : "Vout+"}, Vd=Vd, Is=Is),
-                                SemiRealDiode("D4", {"Vp" : "Vout-", "Vn" : "Vin1"}, Vd=Vd, Is=Is),
 )
